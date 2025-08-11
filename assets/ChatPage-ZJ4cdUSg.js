436fde8f487c44f94b1165497f736c18c5a07576
kubo <braska08@gmail.com>
2025-08-11 13:52:00 -0600

    Updates

--- assets/ChatPage-ZJ4cdUSg.js
+++ assets/ChatPage-ZJ4cdUSg.js
@@ -1,4 +1,4 @@
-import{c as t,j as e,h,u,e as p,r as g,a as j,i as f,B as n,s as N,k as v}from"./index-aihKMXYv.js";import{S as y}from"./scroll-area-CGlM4Ss5.js";/**
+import{c as t,j as e,h,u,e as p,r as g,a as j,i as f,B as n,s as N,k as v}from"./index-pmOBy6ka.js";import{S as y}from"./scroll-area-DrzJds9E.js";/**
  * @license lucide-react v0.503.0 - ISC
  *
  * This source code is licensed under the ISC license.
